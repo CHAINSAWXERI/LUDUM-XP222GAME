f74730983ccc5f540623a6108a7d850e24ad89b7
CHAINSAWXERI <123538769+CHAINSAWXERI@users.noreply.github.com>
2023-10-02 13:06:43 +0300

    54624565436

--- LUDUMDARE_XPXPXPXP/Assets/Scripts/Player System/Gun.cs
+++ LUDUMDARE_XPXPXPXP/Assets/Scripts/Player System/Gun.cs
@@ -1,13 +1,21 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
     public int _amunitionGun;
+    [SerializeField] GameObject ShotFlash;
     [SerializeField] public GameObject _bullet;
     [SerializeField] public Transform _shotpoint;
 
     private float _timeShot;
     public float _starttumeShot;
+
+
+    private void Start()
+    {
+        ShotFlash.SetActive(false);
+    }
 
     void Update()
     {
@@ -17,14 +25,21 @@ public class Gun : MonoBehaviour
             if (Input.GetMouseButton(0) & (_amunitionGun > 0))
             {
                 _amunitionGun--;
+                ShotFlash.SetActive(true);
                 Instantiate(_bullet, _shotpoint.position, transform.rotation);
                 _timeShot = _starttumeShot;
+
+            }
+            else
+            {
+                ShotFlash.SetActive(false);
             }
         }
         else
         {
             _timeShot -= Time.deltaTime;
         }
+        
     }
 
     public void TakeBoxAm(int amun)
